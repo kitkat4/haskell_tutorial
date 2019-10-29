5d53d26dbb8a83261e7c361e353c010d4c249527
Katsumasa Kitajima <k.k4.bky@gmail.com>
2019-10-29 17:01:48 +0900

    commit

--- src/test_recursive.hs
+++ src/test_recursive.hs
@@ -15,3 +15,27 @@ zip' _ [] = []
 zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
 
 
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = (f x y) : zipWith' f xs ys
+
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) = (quickSort [y | y <- xs, y < x]) ++ [x] ++ (quickSort [y | y <- xs, y >= x])
+
+collatz :: Int -> [Int]
+collatz 1 = [1]
+collatz x
+    | even x = x : collatz (x `div` 2)
+    | otherwise = x : collatz (3 * x + 1)
+
+
+
+numLongCollatz :: [Int] -> Int
+numLongCollatz xs = length (filter isLong (map collatz xs))
+                    where isLong ys = length ys > 15
+                                      
+
+                   
