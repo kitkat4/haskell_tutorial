f45f53e187e74fe1e093a9541edfa11946e93cac
Katsumasa Kitajima <k.k4.bky@gmail.com>
2019-10-29 15:05:07 +0900

    commit

---
+++ src/test_recursive.hs
@@ -0,0 +1,17 @@
+take' :: Int -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = (reverse' xs) ++ [x]
+
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+
